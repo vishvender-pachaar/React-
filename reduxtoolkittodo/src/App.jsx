73b1d83f1ff9cb0daab00df966d8abff388931ad
vishvender pachaar <vpachaar2@gmail.com>
2025-03-16 01:56:17 +0530

    redux toolkit learning

---
+++ reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+    <h1>redux toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
